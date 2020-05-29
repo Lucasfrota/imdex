3a57581fc2d357e444f92a48b4a335a41ed91001
Lucas Frota <lucv.frota@gmail.com>
2020-05-28 23:57:32 -0400

    created structure of indexer class

---
+++ imdex/Indexer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Indexer:
+
+    def __init__(self, image_base=np.array([])):
+        pass
+
+    def add_images(self, images, references, redundancy=3):
+        pass
+
+    def query(self, query_text):
+        pass
+
+    def save(self, file_name):
+        pass
