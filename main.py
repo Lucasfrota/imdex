53f2c7fa7d0a44276dfe282364702414dd83180e
Lucas Frota <lucv.frota@gmail.com>
2020-05-29 22:26:09 -0400

    workingon add images function

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from imdex.Captioner import Captioner
+from imdex.Indexer import Indexer
 
 if __name__ == "__main__":
+    
+    idr = Indexer()
+    
     cap = Captioner()
 
     images = [cap.load_image("./sample_img/cat.jpg"), cap.load_image("./sample_img/dog.jpg")]
 
-    result = cap.captionize(images)
+    #result = cap.captionize(images)
+
+    res = idr.add_images(images, "")
+    print(res.shape)
     
-    print('Prediction Caption:')
-    print(result)
+    #print('Prediction Caption:')
+    #print(result)
